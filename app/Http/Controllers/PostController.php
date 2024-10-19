6e1f93768bce28bf93e9caf0316e601672068039
shi006x <tomoka116@icloud.com>
2024-10-19 14:03:30 +0900

    ページネーション データの取得件数制限

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,13 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-       return $post->get();
-    }
-}
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    } 
 
+    public function getByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+}
+}
 ?>
