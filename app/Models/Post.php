0911db83ce4ca64fb62d696de0588ef49031b5b6
shi006x <tomoka116@icloud.com>
2024-10-30 19:03:15 +0900

    ブログ投稿作成画面作成

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,6 +17,10 @@ class Post extends Model
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    public function create()
+    {
+    return view('posts.create');
+    }
 
 }
 ?>
